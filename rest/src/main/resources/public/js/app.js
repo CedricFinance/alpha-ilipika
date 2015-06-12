0a7a9b1e0381b68450c9cdaa5041abf3c77a9295
xeter <mbriend@gmail.com>
2015-06-12 21:36:53 +0200

    Ajout de polymer. On a un composant pour les graphes

--- rest/src/main/resources/public/js/app.js
+++ rest/src/main/resources/public/js/app.js
@@ -1,41 +1,4 @@
 $(function() {
 
-	$.ajax('/list').done(function(data) {
-		console.log(data);
-		showGraph(data);
-	});
-	
-	function showGraph(data) {
-		// create an array with nodes
-		
-		var nodesData = [];
-		var nodesNamesIdMap = {};
-		data.forEach(function(app, index) {
-			nodesData.push({ id: index+1, label: app.name });
-			nodesNamesIdMap[app.name] = index + 1;
-		})
-		var nodes = new vis.DataSet(nodesData);
-	
-		// create an array with edges
-		var edgesData = [];
-		data.forEach(function(app, index) {
-			app.dependencies.forEach(function(dependency){
-				edgesData.push({
-					from: index + 1,
-					to: nodesNamesIdMap[dependency],
-					arrows: 'to'
-				});
-			})
-		});
-		var edges = new vis.DataSet(edgesData);
-	
-		// create a network
-		var container = document.getElementById('mynetwork');
-		var data = {
-			nodes : nodes,
-			edges : edges
-		};
-		var options = {};
-		var network = new vis.Network(container, data, options);
-	}
+
 });
